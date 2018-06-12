0ff5c5426a90f913f522ae3ec45d2e74f3087a4b
HP <ahmed_pop219490@yahoo.com>
2018-06-13 00:30:27 +0200

    :)

---
+++ app/src/main/java/bekya/admin/ActivityOneItem.java
@@ -0,0 +1,190 @@
+package bekya.admin;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+import com.veinhorn.scrollgalleryview.MediaInfo;
+import com.veinhorn.scrollgalleryview.ScrollGalleryView;
+import com.veinhorn.scrollgalleryview.loader.MediaLoader;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by HP on 12/06/2018.
+ */
+
+public class ActivityOneItem extends AppCompatActivity {
+    List<Retrivedata> array;
+    public RecyclerView recyclerView;
+
+    TextView textprice;
+    TextView textname, textdiscrp, textdiscount, textphone, textdate;
+    LinearLayoutManager linearLayoutManager;
+    SwipeRefreshLayout mSwipeRefreshLayout;
+    Retrivedata set;
+    private static final String movieUrl = "http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4";
+    String img1,img2,img3,img4;
+    private ScrollGalleryView scrollGalleryView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activityitem);
+        set=new Retrivedata();
+        array = new ArrayList<>();
+        textname = findViewById(R.id.textname);
+        textdiscrp = findViewById(R.id.textdiscrp);
+        textdiscount = findViewById(R.id.textdiscount);
+        textphone = findViewById(R.id.textphone);
+        textdate = findViewById(R.id.textdate);
+
+        String name = getIntent().getStringExtra("name");
+        String discrption = getIntent().getStringExtra("discrp");
+        String discount = getIntent().getStringExtra("discount");
+        String phone = getIntent().getStringExtra("phone");
+        String date = getIntent().getStringExtra("date");
+        textname.setText(name);
+        textdiscrp.setText(discrption);
+        textdiscount.setText(discount);
+        textphone.setText(phone);
+        textdate.setText(date);
+        getdata(new firebase() {
+            @Override
+            public void Call(Retrivedata r) {
+                img1=r.getImg1();
+                img2=r.getImg2();
+                img3=r.getImg3();
+                List<MediaInfo> infos = new ArrayList<>(images.size());
+
+                for (String url : images) infos.add(MediaInfo.mediaLoader(new PicassoImageLoader(url)));
+
+                scrollGalleryView = (ScrollGalleryView) findViewById(R.id.scroll_gallery_view);
+                scrollGalleryView
+                        .setThumbnailSize(100)
+                        .setZoom(true)
+                        .setFragmentManager(getSupportFragmentManager())
+                        .addMedia(MediaInfo.mediaLoader(new PicassoImageLoader(img1)))
+                        .addMedia(MediaInfo.mediaLoader(new PicassoImageLoader((img2))))
+                        .addMedia(MediaInfo.mediaLoader(new PicassoImageLoader((img3))))
+                        .addMedia(MediaInfo.mediaLoader(new PicassoImageLoader((img4))))
+
+
+                        .addMedia(MediaInfo.mediaLoader(new MediaLoader() {
+                            @Override
+                            public boolean isImage() {
+                                return true;
+                            }
+
+                            @Override
+                            public void loadMedia(Context context, ImageView imageView,
+                                                  MediaLoader.SuccessCallback callback) {
+//                        imageView.setImageBitmap(toBitmap(R.drawable.wallpaper3));
+//                                Picasso.with(context)
+//                                        .load(img1)
+//                                        .fit()
+//                                        .placeholder(R.drawable.no_media)
+//                                        .into(imageView);
+//                                callback.onSuccess();
+                            }
+
+                            @Override
+                            public void loadThumbnail(Context context, ImageView thumbnailView,
+                                                      MediaLoader.SuccessCallback callback) {
+//                                thumbnailView.setImageBitmap(toBitmap(R.drawable.wallpaper3));
+//                                Picasso.with(context)
+//                                        .load(img1)
+//                                        .fit()
+//                                        .placeholder(R.drawable.no_media)
+//                                        .into(thumbnailView);
+//                                callback.onSuccess();
+                            }
+                        }));
+
+            }
+        });
+
+//                .addMedia(MediaInfo.mediaLoader(new DefaultVideoLoader(movieUrl, R.mipmap.default_video)))
+//                .addMedia(infos);
+    }
+    private Bitmap toBitmap(int image) {
+        return ((BitmapDrawable) getResources().getDrawable(image)).getBitmap();
+    }
+
+    private  final ArrayList<String> images = new ArrayList<>(Arrays.asList(
+            img1,
+            img2,
+            img3
+    ));
+
+
+
+
+    public void getdata(final firebase f){
+        String key=getIntent().getStringExtra("key");
+        String child=getIntent().getStringExtra("child");
+
+        DatabaseReference data= FirebaseDatabase.getInstance().getReference().child("Products").child(child);
+        data.orderByChild("img1").equalTo(key).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot child : dataSnapshot.getChildren()) {
+                    if(child.hasChild("img1")) {
+
+                        set.setImg1(child.child("img1").getValue().toString());
+                        array.add(set);
+//                        mAdapter.notifyDataSetChanged();
+                    }
+                    if(child.hasChild("img2")) {
+
+                        set.setImg2(child.child("img2").getValue().toString());
+                        array.add( set);
+//                        mAdapter.notifyDataSetChanged();
+                    }
+                    if(child.hasChild("img3")) {
+
+                        set.setImg3(child.child("img3").getValue().toString());
+                        array.add( set);
+//                        mAdapter.notifyDataSetChanged();
+                    }
+                    if(child.hasChild("img4")) {
+
+                        set.setImg4(child.child("img4").getValue().toString());
+                        array.add( set);
+//                        mAdapter.notifyDataSetChanged();
+                    }
+                    f.Call(set);
+                }
+//                mSwipeRefreshLayout.setRefreshing(false);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+    public interface firebase{
+        void Call(Retrivedata r);
+    }
+
+
+}
---
+++ app/src/main/java/bekya/admin/Adapteritems.java
@@ -0,0 +1,222 @@
+package bekya.admin;
+
+import android.content.Context;
+import android.net.Uri;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by HP on 12/06/2018.
+ */
+
+public class Adapteritems extends RecyclerView.Adapter<Adapteritems.MyViewHolder> implements Filterable {
+    private List<Retrivedata> mArrayList=new ArrayList<>();
+    itemViewinterface itemclick;
+    List<Retrivedata> array=new ArrayList<>();
+    public imgclick btnclick;
+    Context context;
+    public static List<Retrivedata> filteredList = new ArrayList<>();
+
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                filteredList.clear();
+                String charString = charSequence.toString();
+                if (charString.isEmpty()) {
+                    array = mArrayList;
+                } else {
+                    for (Retrivedata androidVersion : mArrayList) {
+                        if (androidVersion.getName().toLowerCase().contains(charString)) {
+                            filteredList.add(androidVersion);}}
+                    array = filteredList;}
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = array;
+                return filterResults;
+            }
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+
+                array = (List<Retrivedata>) filterResults.values;
+
+                notifyDataSetChanged();
+            }
+        };
+
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder  implements View.OnClickListener{
+        public ImageView image,imageedit,imgdelete;
+        CardView itemcard;
+        TextView textname,textdiscraption,textdiscount,textphone,textdate,textadmin;
+        public MyViewHolder(View view) {
+            super(view);
+            image =  view.findViewById(R.id.product_image);
+            textadmin=view.findViewById(R.id.admintext);
+            imageedit=view.findViewById(R.id.imageedit);
+            imgdelete=view.findViewById(R.id.imagedeleteee);
+            textname= view.findViewById(R.id.product_name);
+            textdiscraption= view.findViewById(R.id.discount);
+            textdiscount= view.findViewById(R.id.discraptionn);
+            textphone= view.findViewById(R.id.textphone);
+            textdate=view.findViewById(R.id.textdate);
+            itemcard=view.findViewById(R.id.itemcard);
+            view.setOnClickListener(this);
+
+        }
+
+        @Override
+        public void onClick(View view) {
+            if(itemclick!=null)
+                itemclick.Callback(view,getAdapterPosition());
+
+        }
+    }
+    public Adapteritems( List<Retrivedata> moviesList , Context context) {
+        this.array = moviesList;
+        this.context=context;
+        mArrayList = moviesList;
+        setHasStableIds(true);
+    }
+    public void setClickListener(itemViewinterface itemClickListener) {
+        this.itemclick = itemClickListener;
+    }
+    public void setClickButton(imgclick btnclic){
+        this.btnclick=btnclic;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.product_item, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+
+
+
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, final int position) {
+
+
+        Retrivedata y=array.get(holder.getAdapterPosition());
+
+
+        Boolean admin=y.getAdmin();
+        if(admin){
+            holder.textadmin.setVisibility(View.VISIBLE);
+
+        }else {
+
+        }
+
+        String token=y.getToken();
+//        if(token.equals(Home.token)){
+//            holder.imageedit.setVisibility(View.VISIBLE);
+//            holder.imgdelete.setVisibility(View.VISIBLE);
+//        }else {
+//            holder.imageedit.setVisibility(View.INVISIBLE);
+//            holder.imgdelete.setVisibility(View.INVISIBLE);
+//        }
+
+        String textdate=y.getDate();
+        holder.textdate.setText(textdate);
+
+        String text=y.getName();
+        holder.textname.setText(text);
+
+        String textdiscrp=y.getDiscrption();
+        holder.textdiscraption.setText(textdiscrp);
+
+        String textdiscount=y.getDiscount();
+        holder.textdiscount.setText(textdiscount);
+
+        String textphone=y.getPhone();
+        holder.textphone.setText(textphone);
+
+        String img1 = y.getImg1();
+        String img2 = y.getImg2();
+        String img3 = y.getImg3();
+        String img4 = y.getImg4();
+
+        if(img1!=null) {
+            Uri u = Uri.parse(img1);
+            Picasso.with(context)
+                    .load(u)
+                    .fit()
+                    .placeholder(R.drawable.no_media)
+                    .into(holder.image);
+        }else if(img2!=null){
+            Uri u = Uri.parse(img2);
+            Picasso.with(context)
+                    .load(u)
+                    .fit()
+                    .placeholder(R.drawable.no_media)
+                    .into(holder.image);
+
+        }else if(img3!=null){
+            Uri u = Uri.parse(img3);
+            Picasso.with(context)
+                    .load(u)
+                    .fit()
+                    .placeholder(R.drawable.no_media)
+                    .into(holder.image);
+        }else if(img4!=null){
+            Uri u = Uri.parse(img4);
+            Picasso.with(context)
+                    .load(u)
+                    .fit()
+                    .placeholder(R.drawable.no_media)
+                    .into(holder.image);
+
+        }else {
+            holder.image.setImageResource(R.drawable.no_media);
+        }
+        holder.imageedit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnclick.onClickCallback(view,position);
+            }
+        });
+        holder.imgdelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnclick.onClickdelete(view,position);
+            }
+        });
+
+
+    }
+
+
+
+
+    @Override
+    public int getItemCount() {
+        return array.size();
+    }
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position;
+    }
+}
